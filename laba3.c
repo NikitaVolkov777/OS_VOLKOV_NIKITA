39428f873b3b2897ecfca18f1614b83689164c33
NikitaVolkov777 <volkniknikvolk@gmail.com>
2024-12-04 08:26:00 +0700

    Update laba3.c

--- laba3.c
+++ laba3.c
@@ -1,19 +1,17 @@
 #include <linux/kernel.h>
 #include <linux/module.h> 
 #include <linux/printk.h>
-#include <linux/proc_fs.h>
-#include <linux/uaccess.h>
-#include <linux/version.h>
+#include <linux/init.h>
 
-static int __init Module_init(void) {
+static int __init my_init(void) {
     pr_info("Welcome to the Tomsk State University\n");
     return 0;
 }
 
-static void __exit Module_exit(void) {
+static void __exit my_exit(void) {
     pr_info("Tomsk State University forever!\n");
 }
 
-module_init(Module_init);
-module_exit(Module_exit);
+module_init(my_init);
+module_exit(my_exit);
 MODULE_LICENSE("GPL");
